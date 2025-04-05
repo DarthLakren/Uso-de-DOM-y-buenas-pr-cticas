602b01e6dcaef4f8ad5f3faad32eaa0487b32a48
DarthLakren <vazquezgarciafernandoalexis@gmail.com>
2025-04-04 23:19:55 -0600

    V6
    Mueve la función crearTabla a otro archivo llamado funcionesAvanzadas.js, y agrega el siguiente código al final del documento:

--- funciones.js
+++ funciones.js
@@ -1,3 +1,5 @@
+
+import { crearTablas } from"./funcionesAvanzadas.js";
 function leerDatos(){
     try{
         let filas = document.getElementById("filas").value;
@@ -13,21 +15,4 @@ function leerDatos(){
     }
 }
 
-function crearTablas(filas, columnas){
-    let contenedor = document.getElementById("tabla");
-    contenedor.innerHTML = "";
-    
-    // Crear la tabla
-    let tabla = document.createElement("table");
-    tabla.setAttribute("border", "1");
 
-    for(let x = 0; x<filas; x++){
-        let fila = document.createElement("tr");
-        for(let y = 0; y<columnas; y++){
-            let celda = document.createElement("td");
-            fila.appendChild(celda);
-        }
-        tabla.appendChild(fila);
-    }
-    contenedor.appendChild(tabla);
-}
